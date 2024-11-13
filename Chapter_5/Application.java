8d53eba991ab8766b28f13abfb5d5db880fc42a4
Ange TOSSOU <angetossou.dev@gmail.com>
2024-11-13 06:09:18 +0100

    A Hello World program

---
+++ Chapter_5/Application.java
@@ -0,0 +1,8 @@
+package Java.Java_Tutorial_for_Complete_Beginners.Chapter_5;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
