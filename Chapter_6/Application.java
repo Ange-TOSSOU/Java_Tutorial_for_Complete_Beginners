bb6c171c62e52f79796635b95914570e0d8f4f9c
Ange TOSSOU <angetossou.dev@gmail.com>
2024-11-15 22:03:58 +0100

    Refactor Application code

---
+++ Chapter_6/Application.java
@@ -0,0 +1,22 @@
+package Java.Java_Tutorial_for_Complete_Beginners.Chapter_6;
+
+public class Application {
+    public static void main(String[] args) {
+        short myShort = 847; // 16-bit or 2-byte variable
+        int myInt = 88; // 32-bit or 4-byte variable
+        long myLong = 9797; // 64-bit or 8-byte variable
+
+        float myFloat = 324.3f;
+        double myDouble = 7.3243;
+
+        char myChar = 'y';
+
+        boolean myBoolean = true;
+
+        byte myByte = 127; // 8-bit or 1-byte variable
+
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myLong);
+    }
+}
