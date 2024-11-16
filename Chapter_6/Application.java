73b83098519ee2e617b7a6cbfb0af8d9cb7dbf2e
Ange TOSSOU <angetossou.dev@gmail.com>
2024-11-16 18:32:33 +0100

    Also print non-numeric variable

--- Chapter_6/Application.java
+++ Chapter_6/Application.java
@@ -18,5 +18,18 @@ public class Application {
         System.out.println(myShort);
         System.out.println(myInt);
         System.out.println(myLong);
+        System.out.println();
+
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println();
+
+        System.out.println(myChar);
+        System.out.println();
+
+        System.out.println(myBoolean);
+        System.out.println();
+
+        System.out.println(myByte);
     }
 }
