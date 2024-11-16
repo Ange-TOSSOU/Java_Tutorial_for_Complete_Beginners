dabe41e02000a7cd592a7ecd4ae9b86b6ce9611c
Ange TOSSOU <angetossou.dev@gmail.com>
2024-11-16 19:19:29 +0100

    Discover special characters in Java

---
+++ Chapter_6/SpecialCharacters.java
@@ -0,0 +1,20 @@
+package Java.Java_Tutorial_for_Complete_Beginners.Chapter_6;
+
+public class SpecialCharacters {
+
+    public static void main(String[] args) {
+        // Backspace : \b
+        System.out.println("I'm ha\bungry !");
+
+        // Line feed : \n
+        System.out.println("From here\nTo here");
+
+        // Double quote : \"
+        System.out.println("But do you know the \"so-called\" Joker ?");
+
+        // Single quote : \'
+        char myChar = '\'';
+        System.out.println("I don" + myChar + "t.");
+    }
+
+}
